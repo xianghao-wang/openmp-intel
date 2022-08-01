37d3ab4a2346267947526d346dabe42c85bed685
xianghao <u7321615@anu.edu.au>
2022-08-01 20:18:46 +1000

    07_exercise.c

---
+++ 07_exercise.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <omp.h>
+
+#define MAX_THREADS_N 5
+static long num_steps = 1000000000;
+double step;
+
+int main()
+{
+    int i;
+    int n_threads;
+    double pi;
+    step = 1.0 / (double) num_steps;
+
+    // Set the number of threads.
+    omp_set_num_threads(MAX_THREADS_N);
+
+    double start_time = omp_get_wtime();
+    #pragma omp parallel
+    {  
+        int id = omp_get_thread_num();
+        int i;
+        // Actual threads may be fewer, so we need get the number of running threads
+        int n_actual_threads = omp_get_num_threads();
+        double x;
+        double subsum;
+
+        // Update threads number
+        if (id == 0) n_threads = n_actual_threads;
+
+        // Compute in cyclic
+        for (i = id, subsum = 0.0; i < num_steps; i += n_actual_threads) {
+            x = i * step;
+            subsum += 4.0 / (1.0 + x * x);
+        }
+
+        #pragma omp critical
+            pi += subsum * step;
+    }
+
+    printf("PI is %8.5lf, computed by %3d threads, taking %8.5lf seconds.\n",
+        pi, n_threads, omp_get_wtime() - start_time);
+
+    return 0;
+}
